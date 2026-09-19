5ee23c1f867b2270cb76925ee4610f5f6a483201
Anxious <126321376+WislUniversal@users.noreply.github.com>
2026-09-19 22:25:34 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -10767,6 +10767,7 @@ function Library:CreateWindow(WindowInfo)
     Library.Scheme.Font = WindowInfo.Font
     Library.ToggleKeybind = WindowInfo.ToggleKeybind
     Library.AllowModifiers = if WindowInfo.AllowModifiers ~= nil then WindowInfo.AllowModifiers else false
+    Window.AllowModifiers = Library.AllowModifiers
     Library.GlobalSearch = WindowInfo.GlobalSearch
 
     Library.Animations = WindowInfo.Animations
@@ -11212,7 +11213,6 @@ function Library:CreateWindow(WindowInfo)
 
     --// Window Table \\--
     local Window = {}
-    Window.AllowModifiers = Library.AllowModifiers
     local Fading = false
 
     local function SetUICorner(UICorner, Corner, HalfValue)
